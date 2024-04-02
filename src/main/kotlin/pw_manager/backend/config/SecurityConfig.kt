469aab8cae3d50cf7b1e1c268e28444be37a7f81
kjh0work <kjh-work@naver.com>
2024-04-02 10:34:59 +0900

    CHORE : 네이버 소셜 로그인 관련 정보 추가
    spring security oauth2 client의
    registration과 provider를 추가함

--- src/main/kotlin/pw_manager/backend/config/SecurityConfig.kt
+++ src/main/kotlin/pw_manager/backend/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package pw_manager.backend.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 
@@ -11,8 +12,13 @@ class SecurityConfig {
     @Bean
     open fun filterchain(http: HttpSecurity) : SecurityFilterChain {
 
-        http.csrf{
-            csrf -> csrf.disable()
+        http.csrf{ csrf -> csrf.disable() }
+        http.formLogin { formlogin -> formlogin.disable() }
+        http.httpBasic { httpbasic -> httpbasic.disable() }
+        http.oauth2Login(Customizer.withDefaults())
+
+        http.authorizeHttpRequests {
+            authz -> authz.requestMatchers("/","/oauth2/**","/getTestUser")
         }
 
         return http.build()
