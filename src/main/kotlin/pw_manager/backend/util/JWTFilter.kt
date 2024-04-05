65aa002b241e995478aa0c256f097cf0720b90f2
kjh0work <kjh-work@naver.com>
2024-04-05 13:37:29 +0900

    FIX : jwt필터에서 사용자 권한 설정
    앱에서 jwt토큰을 활용해서 api요청을 보낼 때, 네이버 로그인 페이지로 리다이렉션 되는 버그. jwtfilter에서 사용자 정보를 저장할 때 authorities를 추가함으로 해결

--- src/main/kotlin/pw_manager/backend/util/JWTFilter.kt
+++ src/main/kotlin/pw_manager/backend/util/JWTFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 import pw_manager.backend.entity.Member
@@ -24,6 +25,7 @@ class JWTFilter(
         }
 
         println("authorization now")
+        println("받은 토큰 : ${authorization}")
         val token = authorization.split(" ")[1]
 
         if(jwtUtil.isExpired(token)){
@@ -34,10 +36,11 @@ class JWTFilter(
 
         val username = jwtUtil.getUsername(token)
         //val role = jwtUtil.getRole(token)
+        val authorities = listOf(SimpleGrantedAuthority("ROLE_USER"))
 
         val customOAuth2User = CustomOAuth2User(Member(username))
 
-        val authenticationToken = UsernamePasswordAuthenticationToken(customOAuth2User, null)
+        val authenticationToken = UsernamePasswordAuthenticationToken(customOAuth2User, null,authorities )
 
         SecurityContextHolder.getContext().authentication = authenticationToken
 
